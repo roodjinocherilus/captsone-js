c8b017115353f4b802aaa485a4341fceda725f09
FevenSeyfu <fevenseyfu00@gmail.com>
2023-02-22 00:42:14 +0300

    style cards

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // import _ from 'lodash';
 import './style.css';
 import displayShow from './modules/load-homepage.js';
-import createAPP from './modules/fetch-comment.js';
 
-createAPP();
 displayShow();
