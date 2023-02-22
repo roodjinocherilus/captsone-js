1a74573a19142c877547f59da84976147fa5e5ac
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-02-22 14:05:39 -0300

    Set eslintc and modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,12 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:react/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -17,9 +15,8 @@
         "sourceType": "module"
     },
     "plugins": [
-        "react",
-        "@typescript-eslint"
+        "react"
     ],
     "rules": {
     }
-}
+};
