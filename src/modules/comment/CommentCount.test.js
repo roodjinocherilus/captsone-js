4d507b6c3fc6204da0980f5feb3653eca8e522a2
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-02-25 18:13:18 -0500

    add test to CommentCount

---
+++ src/modules/comment/CommentCount.test.js
@@ -0,0 +1,33 @@
+const CommentCount = require('./CommentCount');
+
+describe('CommentCount', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="items">
+        <div class="comment">Comment 1</div>
+        <div class="comment">Comment 2</div>
+      </div>
+    `;
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  test('returns the correct number of comments', () => {
+    const count = CommentCount();
+    expect(count).toBe(3);
+  });
+
+  test('returns 0 if there are no comments', () => {
+    document.body.innerHTML = '<div id="items"></div>';
+    const count = CommentCount();
+    expect(count).toBe(0);
+  });
+
+  test('returns NaN if the total comments element is not found', () => {
+    document.body.innerHTML = '<div></div>';
+    const count = CommentCount();
+    expect(isNaN(count)).toBe(true);
+  });
+});
